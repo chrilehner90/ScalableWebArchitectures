be283a8cf6babbdafeabc7972cc3da1f8a69f0d2
chri@chri.com <chri@chri.com>
2015-03-19 21:37:54 +0000

    moved client from filter helper into systems that make requests

--- lib/filter_helper.rb
+++ lib/filter_helper.rb
@@ -1,11 +1,9 @@
-require_relative './client'
-
 module FilterHelper
 	protected
-	def self.auth_helper(env)
+	def self.auth_helper(client, env)
 		headers = {}
 		headers["AUTHORIZATION"] = env["HTTP_AUTHORIZATION"] if env["HTTP_AUTHORIZATION"]
 		headers["ACCEPT"] = env["HTTP_ACCEPT"] if env["HTTP_ACCEPT"]
-		return Client.get("http://localhost:9292/user", headers: headers)
+		return client.get("http://localhost:9292/user", headers: headers)
 	end
 end
